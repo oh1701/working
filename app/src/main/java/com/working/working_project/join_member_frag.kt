ff45b1a90e6337c55ef9fd54c0c8cf0ff4dac45f
oh1701 <oh1701@naver.com>
2021-01-27 17:28:12 +0900

    123

---
+++ app/src/main/java/com/working/working_project/join_member_frag.kt
@@ -0,0 +1,11 @@
+package com.working.working_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class join_member_frag : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_member_frag)
+    }
+}
---
+++ app/src/main/java/com/working/working_project/login_main.kt
@@ -0,0 +1,11 @@
+package com.working.working_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login_main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_main)
+    }
+}
