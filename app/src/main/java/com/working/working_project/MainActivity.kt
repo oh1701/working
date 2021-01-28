409f94b8f196ec6ab6d4ca18a3119634a5c1d99d
oh1701 <oh1701@naver.com>
2021-01-28 22:14:19 +0900

    123

--- app/src/main/java/com/working/working_project/MainActivity.kt
+++ app/src/main/java/com/working/working_project/MainActivity.kt
@@ -2,16 +2,88 @@ package com.working.working_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.navigation.NavigationView
 import com.working.working_project.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Retrofit
 
-class MainActivity : AppCompatActivity() {
+val data_type = "JSON"
+val num_Of_rows = 10
+val page_No = 1
+val base_time = 2000
+val base_date = 20210128
+val nx = "55"
+val ny = "127"
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val call = ApiObject.retrofitService.GetWeather(data_type, num_Of_rows, page_No, base_date, base_time, nx, ny)
+        call.enqueue(object : retrofit2.Callback<WEATHER>{
+            override fun onResponse(call: Call<WEATHER>, response: Response<WEATHER>) {
+                if(response.isSuccessful){
+                    Log.d("api", response.body().toString())
+                }
+            }
+
+            override fun onFailure(call: Call<WEATHER>, t: Throwable) {
+            }
+        })
+
+        binding.naviSetting.setOnClickListener {
+            binding.drawerlay.openDrawer(GravityCompat.START)  // 왼쪽에서 화면 나옴
+        }
+
+        binding.naviView.setNavigationItemSelectedListener(this) //네비게이션 아이템 클릭 속성 부여.
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean { //네비게이션 아이템 선택 시
+        var drawer = findViewById<DrawerLayout>(R.id.drawerlay)
+        var d = 0
+
+        when (item.itemId) {
+            R.id.navi_schedule -> Log.d("확인", "확인")
+            R.id.navi_alarm -> Toast.makeText(applicationContext, "네비", Toast.LENGTH_SHORT).show()
+            R.id.navi_weather -> Toast.makeText(applicationContext, "네비", Toast.LENGTH_SHORT).show()
+            R.id.navi_my -> Toast.makeText(applicationContext, "네비", Toast.LENGTH_SHORT).show()
+            R.id.navi_pet -> Toast.makeText(applicationContext, "네비", Toast.LENGTH_SHORT).show()
+            R.id.navi_with -> Toast.makeText(applicationContext, "네비", Toast.LENGTH_SHORT).show()
+            R.id.navi_my_everyday -> Toast.makeText(applicationContext, "네비", Toast.LENGTH_SHORT).show()
+            R.id.navi_Alim -> Toast.makeText(applicationContext, "네비", Toast.LENGTH_SHORT).show()
+
+        }
+
+        drawer.closeDrawers()
+        return false
+    }
+
+    override fun onBackPressed() { //뒤로가기 선택 시
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (binding.drawerlay.isDrawerOpen(GravityCompat.START)) // drawerlayout이 GravityCompat.START 식으로 열려있는 경우
+        {
+            binding.drawerlay.closeDrawers() // 닫아준다
+        } else {
+            super.onBackPressed()
+        }
     }
 }
+
--- app/src/main/java/com/working/working_project/Retrofit.kt
+++ app/src/main/java/com/working/working_project/Retrofit.kt
@@ -1,4 +1,69 @@
 package com.working.working_project
 
-class Retrofit {
+import android.content.ClipData
+import android.view.inspector.IntFlagMapping
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+val retrofit = Retrofit.Builder().baseUrl("http://apis.data.go.kr/1360000/VilageFcstInfoService/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+object ApiObject {
+    val retrofitService: WeatherInterface by lazy {
+        retrofit.create(WeatherInterface::class.java)
+    }
 }
+
+interface WeatherInterface{
+
+    @GET("getVilageFcst?serviceKey=Y%2FPMwCd%2Fy9DOSNXSeH3LU3N9Y4XN3rt12jptcwyTHxqGfE8ZeVDZ%2F0t%2FsCbgexqc2Y657jHsq5QNS32qTluCGA%3D%3D")
+    fun GetWeather(
+        @Query("dataType") data_type : String, //쿼리는 주소 '?' 뒷부분의 속성을 작성할 수 있음.
+        @Query("numOfRows") num_Of_rows : Int, //Query는 직역하면 질의문이라는 의미
+        @Query("pageNo") page_No : Int,
+        @Query("base_date") base_date : Int,
+        @Query("base_time") base_time :Int,
+        @Query("nx") nx : String,
+        @Query("ny") ny:String
+    ): Call<WEATHER> //WEATHER 는 DATA CLASS
+}
+
+data class WEATHER( //활용가이드 응답메세지를 변형시킨것.
+    val response : RESPONSE
+)
+
+data class RESPONSE( //활용가이드 응답메세지를 변형시킨것.
+    val header: HEADER,
+    val body:BODY
+)
+
+data class HEADER( //활용가이드 응답메세지를 변형시킨것.
+    val resultCode : Int,
+    val resultMsg : String
+)
+
+data class BODY( //활용가이드 응답메세지를 변형시킨것.
+    val dataType : String,
+    val items : ITEMS
+)
+
+data class ITEMS( //활용가이드 응답메세지를 변형시킨것.
+    val item : List<ITEM>
+)
+
+data class ITEM( //활용가이드 응답메세지를 변형시킨것.
+    val basedate: Int,
+    val baseTime: Int,
+    val category : String
+
+)
